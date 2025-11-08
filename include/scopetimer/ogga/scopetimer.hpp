fa48c5d01f59918203ca2bb485ed67e45c2701e6
Oscar Friis Gustavsen <oscar@friisweb.dk>
2025-11-08 16:32:17 +0100

    tests implemented, scopetimer fixed. %n functionality added to print for containers.

--- include/scopetimer/ogga/scopetimer.hpp
+++ include/scopetimer/ogga/scopetimer.hpp
@@ -7,26 +7,38 @@
 namespace ogga {
 	class scopetimer {
 	public:
-		scopetimer(std::string_view id) noexcept;
+		scopetimer(std::string_view id, std::string_view suffix) noexcept;
 		~scopetimer() noexcept;
 
 	private:
 		stopwatch timer_;
 		std::ostream* os_;
 		std::string id_;
+		std::string suffix_;
 	};
 
-	inline scopetimer::scopetimer(std::string_view id) noexcept
-	: id_(id) {
+	inline scopetimer::scopetimer(std::string_view id, std::string_view suffix = "ns") noexcept
+	: id_(id), suffix_(suffix), os_(&std::cout) {
 		timer_.start();
 	}
 
 	inline scopetimer::~scopetimer() noexcept {
-		timer_.stop();
-		*os_ << id_ << ": \n" <<
-				"   " << timer_.milliseconds() <<
-				"   " << timer_.microseconds() <<
-				"   " << timer_.nanoseconds() <<
-				std::endl;
+		try {
+			timer_.stop();
+			*os_ << id_ << ": ";
+			*os_ << (suffix_ == "ns"
+						 ? timer_.nanoseconds()
+						 : suffix_ == "us"
+							   ? timer_.microseconds()
+							   : suffix_ == "ms"
+									 ? timer_.milliseconds()
+									 : suffix_ == "s"
+										   ? timer_.seconds()
+										   : suffix_ == "min"
+												 ? timer_.minutes()
+												 : timer_.hours()) << '\n';
+		} catch (std::exception& e) {
+			std::cout << e.what() << std::endl;
+		}
 	}
 }
--- test/scopetimer_test.cpp
+++ test/scopetimer_test.cpp
@@ -5,8 +5,9 @@
 void scopetimer_test() {
 	std::cout << "################# scopetimer Test #################" << "\n\n";
 	{
-		ogga::scopetimer timer("compute");
+		ogga::scopetimer timer("compute", "ms");
 		for (int i{}; i < INT_MAX; i++);
 	}
+	for (int i{}; i < INT_MAX / 10; i++);
 	std::cout << "\n################# scopetimer Test #################" << "\n";
 }
