e3cb6ed01ef843da38bed37a194dc11bc8872bff
0gga <oscar@friisweb.dk>
2025-11-26 13:17:30 +0100

    pathing

--- test/csvlog_test.cpp
+++ test/csvlog_test.cpp
@@ -10,27 +10,27 @@
 
 void csvlog_test() {
 	std::cout << "################# csvlog Test #################" << "\n\n";
-	ogga::csv_log minutelyLog(ogga::csv_log::minutely("minutelyLogs"), "status", "message");
+	ogga::csv_log minutelyLog(ogga::csv_log::minutely("csv_log/minutelyLogs"), "status", "message");
 	minutelyLog.log("INFO", "It's Alive!");
 	minutelyLog.log("WARNING", "Nah just kidding.");
 
-	ogga::csv_log hourlyLog(ogga::csv_log::hourly("hourlyLogs"), "status", "message");
+	ogga::csv_log hourlyLog(ogga::csv_log::hourly("csv_log/hourlyLogs"), "status", "message");
 	hourlyLog.log("INFO", "It's Alive!");
 	hourlyLog.log("WARNING", "Nah just kidding.");
 
-	ogga::csv_log dailyLog(ogga::csv_log::daily("dailyLogs"), "status", "message");
+	ogga::csv_log dailyLog(ogga::csv_log::daily("csv_log/dailyLogs"), "status", "message");
 	dailyLog.log("INFO", "It's Alive!");
 	dailyLog.log("WARNING", "Nah just kidding.");
 
-	ogga::csv_log monthlyLog(ogga::csv_log::monthly("monthlyLogs"), "status", "message");
+	ogga::csv_log monthlyLog(ogga::csv_log::monthly("csv_log/monthlyLogs"), "status", "message");
 	monthlyLog.log("INFO", "It's Alive!");
 	monthlyLog.log("WARNING", "Nah just kidding.");
 
-	ogga::csv_log regularlog("regularLogs/regularLog.csv", "status", "message");
+	ogga::csv_log regularlog("csv_log/regularLogs/regularLog.csv", "status", "message");
 	regularlog.log("INFO", "It's Alive!");
 	regularlog.log("WARNING", "Nah just kidding.");
 
-	std::cout << "Check /bin/logs" << "\n";
+	std::cout << "Check /bin/csv_log" << "\n";
 
 	std::cout << "\n################# csvlog Test #################" << "\n";
 }
